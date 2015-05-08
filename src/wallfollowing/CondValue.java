3fe83350692e07c1be6253ae7a7fb764e45bfe81
dlhinkley <duane@dhwd.com>
2015-05-07 19:07:56 -0700

    Move to netbeans

---
+++ src/wallfollowing/CondValue.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wallfollowing;
+
+/**
+ *
+ * @author dlhinkley
+ */
+ public enum CondValue { WAY_TOO_CLOSE , JUST_RIGHT, WAY_TOO_FAR, NONE };
+
