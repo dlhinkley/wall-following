0020753f9a99a2dd5965e9e1eaf0807acb592147
dlhinkley <duane@dhwd.com>
2015-05-16 14:15:00 -0700

    Works navigating from gui

---
+++ src/wallfollowing/Main.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wallfollowing;
+
+/**
+ *
+ * @author dlhinkley
+ */
+public class Main {
+    
+        static SerialComm serial;  
+        static WallFollowing wallFollowing;
+        
+	public static void main(String[] args) throws Exception {
+            
+                
+                GUI.startGui();
+                SerialComm.startSerial();
+                
+//                 wallFollowing = new WallFollowing();
+//
+//                
+//                while (true ) {
+//                    
+//                    wallFollowing.follow();
+//                    Thread.sleep(4000);
+//
+//                }
+	}
+
+
+
+}
